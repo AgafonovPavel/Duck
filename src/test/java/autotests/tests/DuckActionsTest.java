e379bf3673b6cc65fd523d110e6ca974b86d80b4
Anklav <Anklav.90@mail.ru>
2024-06-19 17:18:58 +0700

    del Ignore test

--- src/test/java/autotests/tests/DuckActionsTest.java
+++ src/test/java/autotests/tests/DuckActionsTest.java
@@ -9,7 +9,6 @@ import com.consol.citrus.annotations.CitrusTest;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Flaky;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Test;
 
@@ -136,102 +135,7 @@ public class DuckActionsTest extends DuckActionsClient {
         validateResponsePayload(runner, duckProperties);
     }
 
-    //Тест-кейсы для /api/duck/action/quak
-    @Test(description = "quak (0, 2)")
-    @Description("Проверка голоса уточки. Кол-во повторов - 0, кол-во кряков в звуке - 2")
-    @CitrusTest
-    @Ignore
-    public void successfulQuakOptionOne(@Optional @CitrusResource TestCaseRunner runner) {
-        duckCreateString(runner, "red", "11", "rubber", "quak", "ACTIVE");
-        extractId(runner);
-        deleteFinally(runner);
-        duckQuack(runner, "${duckId}", "0", "2");
-        validateResponseString(runner,
-                "{\n" +
-                        "  \"sound\": \"\"\n" +
-                        "}");
-    }
 
-    @Test(description = "quak (3, 4)")
-    @Description("Проверка голоса уточки. Кол-во повторов - 3, кол-во кряков в звуке - 2")
-    @Flaky
-    @CitrusTest
-    @Ignore
-    public void successfulQuakOptionTwo(@Optional @CitrusResource TestCaseRunner runner) {
-        duckCreateString(runner, "red", "11", "rubber", "quak", "ACTIVE");
-        extractId(runner);
-        deleteFinally(runner);
-        duckQuack(runner, "${duckId}", "3", "2");
-        validateResponseString(runner,
-                "{\n" +
-                        "  \"sound\": \"quak-quak, quak-quak, quak-quak\"\n" +
-                        "}");
-    }
-
-    @Test(description = "quak (2, 3)")
-    @Description("Проверка голоса уточки. Кол-во повторов - 2, кол-во кряков в звуке - 3")
-    @Flaky
-    @CitrusTest
-    @Ignore
-    public void successfulQuakOptionThree(@Optional @CitrusResource TestCaseRunner runner) {
-        duckCreateString(runner, "red", "11", "rubber", "quak", "ACTIVE");
-        extractId(runner);
-        deleteFinally(runner);
-        duckQuack(runner, "${duckId}", "2", "3");
-        validateResponseString(runner,
-                "{\n" +
-                        "  \"sound\": \"quak-quak-quak, quak-quak-quak\"\n" +
-                        "}");
-    }
-
-    @Test(description = "quak (3, 3)")
-    @Description("Проверка голоса уточки. Кол-во повторов - 3, кол-во кряков в звуке - 3")
-    @CitrusTest
-    @Ignore
-    public void successfulQuakOptionFour(@Optional @CitrusResource TestCaseRunner runner) {
-        duckCreateString(runner, "red", "11", "rubber", "quak", "ACTIVE");
-        extractId(runner);
-        deleteFinally(runner);
-        duckQuack(runner, "${duckId}", "3", "3");
-        validateResponseString(runner,
-                "{\n" +
-                        "  \"sound\": \"quak-quak-quak, quak-quak-quak, quak-quak-quak\"\n" +
-                        "}");
-    }
-
-    @Test(description = "quak (3, 3) DB")
-    @Description("Проверка голоса уточки. Кол-во повторов - 3, кол-во кряков в звуке - 3")
-    @Ignore
-    @CitrusTest
-    public void successfulQuakOptionFourDB(@Optional @CitrusResource TestCaseRunner runner) {
-        runner.$(query(db)
-                .statement("SELECT nvl(max(id), 0)+1 ID FROM DUCK")
-                .extract("ID", "duckId"));
-        deleteFinally(runner);
-        databaseUpdate(runner,
-                "INSERT INTO DUCK (id, color, height, material, sound, wings_state)\n" +
-                        "VALUES (${duckId}, 'orange', 10.0, 'rubber', 'quak','ACTIVE');");
-        duckQuack(runner, "${duckId}", "3", "3");
-        validateResponseString(runner,
-                "{\n" +
-                        "  \"sound\": \"quak-quak-quak, quak-quak-quak, quak-quak-quak\"\n" +
-                        "}");
-    }
-
-    @Test(description = "quak (3, 0)")
-    @Description("Проверка голоса уточки. Кол-во повторов - 3, кол-во кряков в звуке - 0")
-    @Ignore
-    @CitrusTest
-    public void successfulQuakOptionFive(@Optional @CitrusResource TestCaseRunner runner) {
-        duckCreateString(runner, "red", "11", "rubber", "quak", "ACTIVE");
-        extractId(runner);
-        deleteFinally(runner);
-        duckQuack(runner, "${duckId}", "3", "0");
-        validateResponseString(runner,
-                "{\n" +
-                        "  \"sound\": \"\"\n" +
-                        "}");
-    }
 
     //Тест-кейсы для /api/duck/action/swim
     @Test(description = "swim")
